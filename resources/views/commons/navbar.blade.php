9446063a22f1030120a06e35b7719a9711658e89
meh <meh>
2018-06-08 13:09:21 +0000

    for pushing

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,6 +13,8 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>{!! link_to_route('tasks.create', 'Create New') !!}</li>
+                    <li><a>Signup</a></li>
+                    <li><a>Login</a></li>
                 </ul>
             </div>
         </div>
